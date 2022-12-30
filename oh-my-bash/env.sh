f1852b25d6f764b43686583cc6360a878a947a55
Jesse Kaufman <jesse@jessekaufman.com>
2022-12-29 19:58:42 -0600

    Add Cargo and Homebrew to $PATH

--- oh-my-bash/env.sh
+++ oh-my-bash/env.sh
@@ -1,3 +1,8 @@
-PATH=./vendor/bin:~/.config/composer/vendor/bin:$PATH
+
+export PATH=/opt/homebrew/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.config/composer/vendor/bin:$PATH
+export PATH=./vendor/bin:$PATH
+
 
 COLORTERM=1
