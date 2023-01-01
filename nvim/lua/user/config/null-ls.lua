20e615ee93ba8e706e8d008ec7048091fea3f273
Jesse Kaufman <jesse@jessekaufman.com>
2023-01-01 13:38:08 -0600

    Fix most LSP issues

--- nvim/lua/user/config/null-ls.lua
+++ nvim/lua/user/config/null-ls.lua
@@ -9,20 +9,30 @@ end
 local b = null_ls.builtins
 
 null_ls.setup({
-	-- debug = true,
+	debug = true,
 	sources = {
+		-- Diagnostics/Linters
 		b.diagnostics.phpcs,
 		b.diagnostics.luacheck,
-		-- b.diagnostics.styleint,
+		b.diagnostics.cpplint,
 		b.diagnostics.php,
+		-- b.diagnostics.styleint,
+
+		-- Formatters
 		b.formatting.stylua,
 		b.formatting.phpcbf,
 		b.formatting.prettier,
 		b.formatting.shfmt,
 		b.formatting.sqlformat,
-		b.diagnostics.cpplint,
+		b.formatting.uncrustify.with({
+			-- args = { "-c", "~/.config/uncrustify.cfg" },
+			extra_args = {
+				"-c",
+				os.getenv( "HOME" ) .. "/.config/uncrustify.cfg",
+				-- require("lspconfig.util").path.join(vim.loop.cwd(), "uncrustify.cfg"),
+			}, -- for neovim/neovim repo
+		}),
 	},
-	on_attach = require('user.src.lsp-handler').on_attach,
-	capatilities = require('user.src.lsp-handler').get_capabilities(),
+	on_attach = require("user.src.lsp-handler").on_attach,
+	capatilities = require("user.src.lsp-handler").get_capabilities(),
 })
-
