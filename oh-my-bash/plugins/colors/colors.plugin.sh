9ea67443e3fb828dae6b9b29b3eae6b742c2e5de
Jesse Kaufman <jesse@jessekaufman.com>
2023-01-05 16:10:29 -0600

    Remove dircolors config

--- oh-my-bash/plugins/colors/colors.plugin.sh
+++ oh-my-bash/plugins/colors/colors.plugin.sh
@@ -7,9 +7,6 @@
 if command -v vivid &>/dev/null; then
     # Use vivid for setting colors
     LS_COLORS="$(vivid generate one-dark)"
-else
-    # Fall back to dircolors
-    eval "$(dircolors ~/.config/dircolors)"
 fi
 
 export LS_COLORS
