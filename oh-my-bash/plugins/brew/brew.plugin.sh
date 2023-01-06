82d534879bf193a0d5ae9f4051d5f1d37b4fd117
Jesse Kaufman <jesse@jessekaufman.com>
2023-01-06 10:32:18 -0600

    Fix Brew paths

--- oh-my-bash/plugins/brew/brew.plugin.sh
+++ oh-my-bash/plugins/brew/brew.plugin.sh
@@ -7,21 +7,16 @@
 # Hide hints.
 export HOMEBREW_NO_ENV_HINTS=1
 
-# Find Homebrew bin dir.
+# Find Homebrew dir.
 if [ -d "/opt/homebrew" ]; then
-    export BREW_PATH="/opt/homebrew/bin"
+    export BREW_PATH="/opt/homebrew"
 elif [ -d "/home/linuxbrew/.linuxbrew/" ]; then
-    export BREW_PATH="/home/linuxbrew/.linuxbrew/bin"
+    export BREW_PATH="/home/linuxbrew/.linuxbrew"
 elif [ -d "/usr/local" ]; then
-    export BREW_PATH="/usr/local/bin"
+    export BREW_PATH="/usr/local"
 fi
 
-# Add Homebrew bin dir to path (if it isn't already).
-if [[ ":$PATH:" != *":$BREW_PATH:"* ]]; then
-    export PATH=$BREW_PATH:$PATH
-fi
-
-# Add aliases for all GNU apps in the coreutils cask bin dir
+# Add aliases for all GNU apps in the coreutils cask dir
 for FILE in $(ls -1 "${BREW_PATH}/"); do
 
     # Only act on files that start with g.
@@ -39,6 +34,3 @@ for FILE in $(ls -1 "${BREW_PATH}/"); do
     # alias ${NEWFILE}="${FILE}"
     alias $(echo $FILE | sed 's/g//')="${FILE}"
 done
-
-export PATH="/opt/homebrew/opt/php@7.4/bin:$PATH"
-export PATH="/opt/homebrew/opt/php@7.4/sbin:$PATH"
--- oh-my-bash/plugins/env/env.plugin.sh
+++ oh-my-bash/plugins/env/env.plugin.sh
@@ -12,11 +12,26 @@ GLX_PATHS=""
 # Add Homebrew to path (if it isn't already).
 #
 
-# Check if directory exists.
-if [[ ":$BREW_PATH:" != "::" ]] &&            # Variable is set. \
-    [[ -d $BREW_PATH ]] &&                    # Directory exists. \
-    [[ ":$PATH:" != *":$BREW_PATH:"* ]]; then # Isn't in path already.
-    GLX_PATHS="$BREW_PATH:$GLX_PATHS"
+# Variable is set.
+if [[ ":$BREW_PATH:" != "::" ]] && \
+    # Brew bin dir exists.
+    [[ -d "$BREW_PATH/bin" ]]; then
+
+        # Brew bin dir isn't in path already.
+        if [[ ":$PATH:" != *":$BREW_PATH/bin:"* ]]; then \
+            # Prepend bin dir to local var of paths
+            GLX_PATHS="$BREW_PATH/bin:$GLX_PATHS"
+        fi
+
+        php_path="$BREW_PATH/php@7.4" # TODO: Remove hardcoded ver
+
+        # Add PHP if it exists
+        if [[ -d $php_path ]]; then
+            # Add PHP bin dir
+            export PATH="$php_path/bin:$PATH"
+            # Add PHP sbin dir
+            export PATH="$php_path/sbin:$PATH"
+        fi
 fi
 
 #
