fceaf947690399ea89ef9455f067700a0b8cc535
Jesse Kaufman <jesse@jessekaufman.com>
2023-01-10 16:40:57 -0600

    Fix permissions

--- oh-my-bash/plugins/colors/colors.plugin.sh
+++ oh-my-bash/plugins/colors/colors.plugin.sh
@@ -6,7 +6,7 @@
 
 if command -v vivid &>/dev/null; then
     # Use vivid for setting colors
-    LS_COLORS="$(vivid generate one-dark)"
+    LS_COLORS="$(vivid generate "$HOME/.config/vivid/themes/glx.yml")"
 fi
 
 export LS_COLORS
