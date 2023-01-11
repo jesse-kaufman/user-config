eca9ec2854ba7359dbeadd1789b9a51e3c422e46
jesse-kaufman <jesse@jessekaufman.com>
2023-01-11 17:26:23 -0600

    OMB tweaks

--- oh-my-bash/aliases/dev-tools.aliases.sh
+++ oh-my-bash/aliases/dev-tools.aliases.sh
@@ -12,4 +12,9 @@ alias shfmt='shfmt --indent=0 --case-indent --binary-next-line --space-redirects
 
 alias stylua='stylua --search-parent-directories'
 
-alias yamlfmt='yamlfmt --config=~/.config/yamlfmt.yaml'
+alias yamlfmt="yamlfmt -conf=\$HOME/.config/yamlfmt.yml"
+alias yamllint="yamllint -c \$HOME/.config/yamllint.yml"
+
+alias phpstan='phpstan --memory-limit=512M'
+
+alias phpcs='phpcs --config-set installed_paths ./vendor/phpcompatibility/php-compatibility,./vendor/phpcsstandards/phpcsdevtools,./vendor/phpcsstandards/phpcsextra,./vendor/phpcsstandards/phpcsutils,./vendor/wp-coding-standards/wpcs,/Users/glandix/.composer/vendor/phpcompatibility/php-compatibility,/Users/glandix/.composer/vendor/phpcsstandards/phpcsdevtools,/Users/glandix/.composer/vendor/phpcsstandards/phpcsextra,/Users/glandix/.composer/vendor/phpcsstandards/phpcsutils,/Users/glandix/.composer/vendor/wp-coding-standards/wpcs,'
