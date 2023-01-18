ad52afcddb99c77302cc468c201e957d4075fdff
jesse-kaufman <jesse@jessekaufman.com>
2023-01-18 12:31:41 -0600

    Tweak NeoVim aliases

--- oh-my-bash/aliases/vim.aliases.sh
+++ oh-my-bash/aliases/vim.aliases.sh
@@ -1,11 +1,14 @@
+# shellcheck shell=bash
+
 #
 # vim/neovim aliases
 #
 
+alias nvim='nvim -p'
+alias v='nvim'
+alias n='nvim'
 alias nw='nvim ~/fo/inc/woocommerce.php'
 alias na='nvim ~/fo/inc/admin.php'
 alias nf='nvim ~/fo/functions.php'
 alias ndevp='nvim /var/log/php/tgdev1.transitionsgroup.net-error_log'
 alias ndeva='nvim /var/log/apache2/tgdev1.transitionsgroup.net-error_log'
-alias v='nvim'
-alias n='nvim'
