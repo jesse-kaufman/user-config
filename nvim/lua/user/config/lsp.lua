0047190726406ca43d94053c1441e5fac9a61983
jesse-kaufman <jesse@jessekaufman.com>
2023-01-19 17:16:55 -0600

    Add sumneko_lua and tweak setup calls

--- nvim/lua/user/config/lsp.lua
+++ nvim/lua/user/config/lsp.lua
@@ -2,12 +2,44 @@ local lspconfig = require('lspconfig')
 
 local ensure_installed = {
     'vimls',
+    'sumneko_lua',
 }
 
+local flags = require('user.src.lsp-handler').flags
+local capabilities = require('user.src.lsp-handler').capabilities
+local on_attach = require('user.src.lsp-handler').on_attach
+
 lspconfig.vimls.setup({
-    flags = require('user.src.lsp-handler').flags,
-    capabilities = require('user.src.lsp-handler').capabilities,
-    on_attach = require('user.src.lsp-handler').on_attach,
+    flags = flags,
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require('lspconfig').sumneko_lua.setup({
+    settings = {
+        Lua = {
+            semantic = {
+                -- enable = false,
+                keyword = false,
+                variable = false,
+            },
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
 -- lspconfig.phpactor.setup({})
 
@@ -19,9 +51,9 @@ lspconfig.intelephense.setup({
             },
         },
     },
-    flags = require('user.src.lsp-handler').lsp_flags,
-    capabilities = require('user.src.lsp-handler').capabilities,
-    on_attach = require('user.src.lsp-handler').on_attach,
+    flags = flags,
+    capabilities = capabilities,
+    on_attach = on_attach,
 })
 vim.lsp.set_log_level('debug')
 
