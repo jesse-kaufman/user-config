af590a5ffc51a7a98377213365310a53399e71c7
jesse-kaufman <jesse@jessekaufman.com>
2023-02-13 15:34:00 -0600

    Use ~/.composer/vendor for stubs/sniffs

--- bin/setup_phpcs.sh
+++ bin/setup_phpcs.sh
@@ -4,7 +4,8 @@ current_phpcs=$(which phpcs)
 
 severity=1
 
-installed_paths="./vendor/phpcompatibility/php-compatibility,./vendor/phpcsstandards/phpcsdevtools,./vendor/phpcsstandards/phpcsextra,./vendor/phpcsstandards/phpcsutils,./vendor/wp-coding-standards/wpcs,$HOME/.composer/vendor/phpcompatibility/php-compatibility,$HOME/.composer/vendor/phpcsstandards/phpcsdevtools,$HOME/.composer/vendor/phpcsstandards/phpcsextra,$HOME/.composer/vendor/phpcsstandards/phpcsutils,$HOME/.composer/vendor/wp-coding-standards/wpcs,"
+# installed_paths="./vendor/phpcompatibility/php-compatibility,./vendor/phpcsstandards/phpcsdevtools,./vendor/phpcsstandards/phpcsextra,./vendor/phpcsstandards/phpcsutils,./vendor/wp-coding-standards/wpcs,$HOME/.composer/vendor/phpcompatibility/php-compatibility,$HOME/.composer/vendor/phpcsstandards/phpcsdevtools,$HOME/.composer/vendor/phpcsstandards/phpcsextra,$HOME/.composer/vendor/phpcsstandards/phpcsutils,$HOME/.composer/vendor/wp-coding-standards/wpcs,"
+installed_paths="$HOME/.composer/vendor/phpcompatibility/php-compatibility,$HOME/.composer/vendor/phpcsstandards/phpcsdevtools,$HOME/.composer/vendor/phpcsstandards/phpcsextra,$HOME/.composer/vendor/phpcsstandards/phpcsutils,$HOME/.composer/vendor/wp-coding-standards/wpcs,"
 
 echo -e "Setting installed_paths for $current_phpcs:\n"
 $current_phpcs --config-set installed_paths "$installed_paths"
